351b0c6d63419baa74edf4a2e315f861b7354c57
r6144 <rainy6144@gmail.com>
2011-03-26 23:53:40 +0800

    More stuff in the mmap manager.

--- radeon/mmap_manager.cpp
+++ radeon/mmap_manager.cpp
@@ -17,23 +17,23 @@
    account. */
 namespace mmap_manager_cpp {
 
-inline bool is_whole_pages(uintptr_t size) {
+inline bool is_whole_pages(size_t size) {
     return (size & PAGE_OFFSET_MASK) == 0;
 }
 
-inline uintptr_t round_up_to_whole_pages(uintptr_t size) {
-    uintptr_t rounded_size = (size + (PAGE_SIZE - 1)) & ~PAGE_OFFSET_MASK;
+inline size_t round_up_to_whole_pages(size_t size) {
+    size_t rounded_size = (size + (PAGE_SIZE - 1)) & ~PAGE_OFFSET_MASK;
     assert(rounded_size >= size);
     return rounded_size;
 }
 
 class FreeBlock: boost::noncopyable {
 public:
-    explicit FreeBlock(uintptr_t size);
-    FreeBlock(uintptr_t addr, uintptr_t size);
+    explicit FreeBlock(size_t size);
+    FreeBlock(uintptr_t addr, size_t size);
     ~FreeBlock();
-    uintptr_t size() { return m_size; }
-    uintptr_t steal(uintptr_t ssize);
+    size_t size() { return m_size; }
+    uintptr_t steal(size_t ssize);
 
     struct size_lt {
 	// Blocks having the same size but different addresses must be able to coexist, so they
@@ -44,10 +44,11 @@ public:
 	}
     };
 private:
-    uintptr_t m_addr, m_size;
+    uintptr_t m_addr;
+    size_t m_size;
 }
 
-FreeBlock::FreeBlock(uintptr_t size): m_size(size) {
+FreeBlock::FreeBlock(size_t size): m_size(size) {
     assert(is_whole_pages(size));
     void *result = mmap(0, size, PROT_NONE, MAP_ANONYMOUS | MAP_PRIVATE, -1, 0);
     assert(result != MAP_FAILED);
@@ -60,7 +61,7 @@ FreeBlock::FreeBlock(uintptr_t size): m_size(size) {
 
 /* NOTE: MAP_FIXED mmap()'s usually follow munmap() at the same address.  They risk a race with
    another thread mmap'ing the same address in the meantime, so failure is always possible. */
-FreeBlock::FreeBlock(uintptr_t addr, uintptr_t size): m_addr(addr), m_size(size) {
+FreeBlock::FreeBlock(uintptr_t addr, size_t size): m_addr(addr), m_size(size) {
     assert(is_whole_pages(size));
     void *result = mmap(static_cast<void *>(addr), size, PROT_NONE, MAP_ANONYMOUS | MAP_PRIVATE | MAP_FIXED, -1, 0);
     assert(result == static_cast<void *>(addr)); // FIXME: might fail due to a race
@@ -72,7 +73,7 @@ FreeBlock::~FreeBlock() {
     assert(result == 0);
 }
 
-uintptr_t FreeBlock::steal(uintptr_t ssize) {
+uintptr_t FreeBlock::steal(size_t ssize) {
     assert(is_whole_pages(ssize)); assert(ssize <= m_size);
     ret = m_addr;
     int result = munmap((void *) m_addr, ssize); assert(result == 0);
@@ -81,28 +82,65 @@ uintptr_t FreeBlock::steal(uintptr_t ssize) {
     // NOTE: The caller should deallocate this FreeBlock if its size becomes zero
 }
 
-// Owns the FreeBlocks
-/* FIXME: Does not yet combine adjacent free blocks.  This should not worsen worst-case address-space consumption too much, though. */
-/* FIXME: Does not yet keep track of used blocks, either.  Actual mapping/unmapping is performed by
- * the caller, which is not very good encapsulation. */
+/* Owns the FreeBlocks, and will thus unmap them upon freeing.  The mapped blocks are owned by the caller of map/unmap, though. */
 class MMapManager: boost::noncopyable {
 public:
     MMapManager();
-    uintptr_t get_map_addr(uintptr_t size);
-    void free_map_addr(uintptr_t addr, uintptr_t size);
+    void *map(size_t len, int prot, int flags, int fd, off_t offset);
+    void unmap(void *addr, size_t len);
 private:
     boost::ptr_set<FreeBlock, FreeBlock::size_lt> m_free_blks;
-    uintptr_t m_next_alloc_size;
-    static const max_alloc_size = 256 * 1024 * 1024;
+    size_t m_last_alloc_size;
+    static const size_t max_alloc_size = 256 * 1024 * 1024;
+    void *find_free_addr(size_t size);
+    void *try_map_from_blocks(size_t size, int prot, int flags, int fd, off_t offset);
 };
 
-MMapManager::MMapManager(): m_next_alloc_size(16 * PAGE_SIZE) {}
+MMapManager::MMapManager(): m_last_alloc_size(0) {}
 
-uintptr_t MMapManager::get_map_addr(uintptr_t size) {
-    
+/* Returns MAP_FAILED upon failure */
+void *MMapManager::find_free_addr(size_t size) {
+    // FIXME
 }
 
-void MMapManager::free_map_addr(uintptr_t addr, uintptr_t size) {
+/* If we fail due to any reason, return MAP_FAILED */
+void *MMapManager::try_map_from_blocks(size_t size, int prot, int flags, int fd, off_t offset) {
+    void *addr = find_free_addr(size);
+    if (addr == MAP_FAILED) {
+	if (size > max_alloc_size) return MAP_FAILED;
+	size_t alloc_size = m_last_alloc_size;
+	alloc_size = std::max(alloc_size, 16 * PAGE_SIZE);
+	while (alloc_size < size && alloc_size < max_alloc_size) alloc_size <<= 1;
+	assert(size <= alloc_size);
+	m_free_blks.insert(new FreeBlock(alloc_size));
+	m_last_alloc_size = alloc_size;
+	addr = find_free_addr(size);
+    }
+    assert(addr != MAP_FAILED);
+    void *result = mmap(addr, size, prot, flags | MAP_FIXED, fd, offset);
+    if (result == MAP_FAILED)
+	std::cerr << "Fixed mmap failed in try_map_from_blocks (addr=" << addr << ", size=" << size << "); race?\n";
+    return result;
+}
+
+void *MMapManager::map(size_t len, int prot, int flags, int fd, off_t offset) {
+    size_t size = round_up_to_whole_pages(len);
+    void *result = try_map_from_blocks(size, prot, flags, fd, offset);
+    if (result == MAP_FAILED) {
+	/* The mapping is so large that the number of such mappings is unlikely to be too many.  We thus allocate directly. */
+	std::clog << "Direct allocation of size " << size << '\n';
+	result = mmap(0, size, prot, flags, fd, offset);
+    }
+    return result;
+}
+
+/* FIXME: Does not yet keep track of the mappings */
+void MMapManager::unmap(void *addr, size_t len) {
+    size_t size = round_up_to_whole_pages(len);
+    m_free_blks.insert(new FreeBlock(addr, len));
+    // FIXME: catch exceptions due to races
+    // FIXME: combine adjacent free blocks by calling mremap()
+}
     
 } // end of namespace mmap_manager_cpp
 
